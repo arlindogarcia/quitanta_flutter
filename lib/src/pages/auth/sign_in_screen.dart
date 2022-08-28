2e9377128110be0280bc8d46f2e01030657e5893
Arlindo Garcia Gomes Neto <arlindogarciagomesneto70@gmail.com>
2022-08-28 19:05:19 -0300

    Feat: top menu base, campo pesquisa e categorias

--- lib/src/pages/auth/sign_in_screen.dart
+++ lib/src/pages/auth/sign_in_screen.dart
@@ -1,9 +1,9 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
-import 'package:quitanta_flutter/src/auth/components/custom_text_field.dart';
-import 'package:quitanta_flutter/src/auth/sign_up_screen.dart';
-import 'package:quitanta_flutter/src/base/base_screen.dart';
 import 'package:quitanta_flutter/src/config/custom_colors.dart';
+import 'package:quitanta_flutter/src/pages/auth/components/custom_text_field.dart';
+import 'package:quitanta_flutter/src/pages/auth/sign_up_screen.dart';
+import 'package:quitanta_flutter/src/pages/base/base_screen.dart';
 
 class SignInScreen extends StatelessWidget {
   const SignInScreen({Key? key}) : super(key: key);
---
+++ lib/src/pages/home/home_tab.dart
@@ -0,0 +1,138 @@
+import 'package:badges/badges.dart';
+import 'package:flutter/material.dart';
+import 'package:quitanta_flutter/src/config/custom_colors.dart';
+import 'package:quitanta_flutter/src/pages/home/components/category_tile.dart';
+
+class HomeTab extends StatefulWidget {
+  HomeTab({Key? key}) : super(key: key);
+
+  @override
+  State<HomeTab> createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
+  List<String> categories = [
+    'Frutas',
+    'Grãos',
+    'Verduras',
+    'Temperos',
+    'Cereais',
+  ];
+
+  String selectedCategory = 'Frutas';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // AppBar
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: Text.rich(
+          TextSpan(
+            style: const TextStyle(fontSize: 30),
+            children: [
+              TextSpan(
+                text: 'Quitanda',
+                style: TextStyle(
+                  color: CustomColors.customSwatchColor,
+                ),
+              ),
+              TextSpan(
+                text: 'Virtual',
+                style: TextStyle(
+                  color: CustomColors.customContrastColor,
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 15,
+              right: 15,
+            ),
+            child: GestureDetector(
+              onTap: () {},
+              child: Badge(
+                badgeColor: CustomColors.customContrastColor,
+                badgeContent: const Text(
+                  '2',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
+                ),
+                child: Icon(
+                  Icons.shopping_cart,
+                  color: CustomColors.customSwatchColor,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          // Campo de pesquisa
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ),
+            child: TextFormField(
+              decoration: InputDecoration(
+                isDense: true,
+                filled: true,
+                hintText: 'Pesquise aqui...',
+                hintStyle: TextStyle(
+                  color: Colors.grey.shade400,
+                  fontSize: 14,
+                ),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: CustomColors.customContrastColor,
+                  size: 21,
+                ),
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(60),
+                  borderSide: const BorderSide(
+                    width: 0,
+                    style: BorderStyle.none,
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          // Categorias
+          Container(
+            padding: const EdgeInsets.only(left: 25),
+            height: 40,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (_, index) {
+                return CategoryTile(
+                  category: categories[index],
+                  isSelected: categories[index] == selectedCategory,
+                  onPressed: () {
+                    setState(() {
+                      selectedCategory = categories[index];
+                    });
+                  },
+                );
+              },
+              separatorBuilder: (_, index) => const SizedBox(width: 10),
+              itemCount: categories.length,
+            ),
+          ),
+
+          // Grid
+        ],
+      ),
+    );
+  }
+}
