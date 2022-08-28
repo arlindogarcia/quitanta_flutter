4b4334675ccb1e76889e211795bd848739eaca57
Arlindo Garcia Gomes Neto <arlindogarciagomesneto70@gmail.com>
2022-08-28 14:39:58 -0300

    Feat: tela de cadastro

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,6 +1,7 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:quitanta_flutter/src/auth/components/custom_text_field.dart';
+import 'package:quitanta_flutter/src/auth/sign_up_screen.dart';
 import 'package:quitanta_flutter/src/config/custom_colors.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -156,7 +157,13 @@ class SignInScreen extends StatelessWidget {
                       SizedBox(
                         height: 50,
                         child: OutlinedButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(builder: (c) {
+                                return SignUpScreen();
+                              }),
+                            );
+                          },
                           style: OutlinedButton.styleFrom(
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(18),
