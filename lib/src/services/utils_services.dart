36ebd72e2a3fc7065bc6603596c9027b56fe3261
Arlindo Garcia Gomes Neto <arlindogarciagomesneto70@gmail.com>
2022-09-01 00:18:48 -0300

    Feat: função de criar Toast

--- lib/src/services/utils_services.dart
+++ lib/src/services/utils_services.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
@@ -16,5 +18,49 @@ class UtilsServices {
     DateFormat dateFormat = DateFormat.yMd('pt_BR').add_Hm();
 
     return dateFormat.format(dateTime);
+  }
+
+  void showToast({
+    required FToast fToast,
+    required String message,
+    bool isError = false,
+    // ignore: avoid_init_to_null
+    IconData? icon = null,
+  }) {
+    Widget toast = Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25.0),
+        color: isError ? Colors.red : Colors.white,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Visibility(
+            visible: icon == null ? false : true,
+            child: Column(
+              children: [
+                Icon(icon),
+                const SizedBox(
+                  width: 12.0,
+                ),
+              ],
+            ),
+          ),
+          Text(
+            message,
+            style: TextStyle(
+              color: isError ? Colors.white : Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    fToast.showToast(
+      child: toast,
+      toastDuration: const Duration(seconds: 2),
+      gravity: ToastGravity.BOTTOM,
+    );
   }
 }
